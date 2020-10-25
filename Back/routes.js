16057fa01cdbcc904aca7dfa614bb1bf04e9816a
Zyon <zyonamadeus01@gmail.com>
2020-10-25 20:02:01 -0300

    Adicionado express e começado integração com react

--- Back/routes.js
+++ Back/routes.js
@@ -1,3 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://mongo/Vivaldi',
+    (err, conn) => {
+        if(err) throw err;
+        const db = conn.db();
+        conn.close();
+    });
+
 let http =require('http'),
     path =require('path'),
     express = require('express'),
@@ -8,7 +17,10 @@ app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended :false}));
 
-app.get('/app', (req,res)=>{
-    res.render('app')
-})
+app.get('/Vivaldi/items', (req,res)=>{
+    let q = (req.query && req.query.busca) ? req.query.busca:'';
+    UserDAO.find(q).then((items) =>{
+        res.json(items);
+    });
+});
 app.listen(3000);
