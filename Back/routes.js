e32f4e14224486eeced0fcab204bd4893d2c2dc5
Zyon <zyonamadeus01@gmail.com>
2020-10-26 07:13:55 -0300

    Implementado parte da integração com o react e mongoDB

--- Back/routes.js
+++ Back/routes.js
@@ -1,26 +1,59 @@
 const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
 
-MongoClient.connect('mongodb://mongo/Vivaldi',
-    (err, conn) => {
-        if(err) throw err;
-        const db = conn.db();
-        conn.close();
-    });
-
-let http =require('http'),
-    path =require('path'),
-    express = require('express'),
-    app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'view'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended :false}));
-
-app.get('/Vivaldi/items', (req,res)=>{
-    let q = (req.query && req.query.busca) ? req.query.busca:'';
-    UserDAO.find(q).then((items) =>{
-        res.json(items);
-    });
+MongoClient.connect(/*LINK DO BANCO*/, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db(/*NOME DO BANCO*/)
+    const usersCollection = db.collection('users')
+    const contentsCollection = db.collection('contents')
 });
-app.listen(3000);
+
+app.listen(3021, function(){
+    console.log('Ouvindo em 3021')
+});
+//função teste
+app.get('/', (req, res)=>{
+    res.send('fala brow2')
+});
+//buscar ususarios 
+app.get('/users', (req, res) =>{
+    db.collection('users').find().toArray()
+        .then(results =>{
+            console.log(results)
+        })
+        .catch(error => console.error(error))
+});
+//cadastrar usuario
+app.post('/users', (req, res) =>{
+    usersCollection.insertOne(req.body)
+        .then(result =>{
+            console.log(result);
+        })
+        .catch(error => console.error(error))
+});
+//postar imagem
+app.post('/images', (req, res)=>{
+
+});
+//postar algum texto
+app.post('/content', (req, res)=>{
+    contentsCollection.insertOne(req.body)
+        .then(result =>{
+            console.log(result);
+        })
+        .catch(error => console.error(error))
+});
+//buscar imagem
+app.get('/images', (req, res)=>{
+
+});
+//buscar algum texto
+app.get('/content', (req, res)=>{
+    db.collection('content').find().toArray()
+        .then(results =>{
+            console.log(results)
+        })
+        .catch(error => console.error(error))
+});
