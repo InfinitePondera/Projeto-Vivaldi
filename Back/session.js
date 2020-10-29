c4a074f32fb5c92061a7d1a36f1ffb049a45edc3
unknown <gabrielantalsilva@gmail.com>
2020-10-28 22:19:55 -0300

    feito esboço de sessions

---
+++ Back/session.js
@@ -0,0 +1,28 @@
+var session = require('express-session');
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+app.use(session({
+    secret:'s3crets'
+}));
+
+
+router.post('/', function(req, res, next){
+    let login= req.body.name;
+
+    if(login===/**/&&password===/* */){
+        req.session.name =  req.body.name;
+        return ;
+    } 
+});
+
+
+router.get('/', function(req,res,next){
+    if(req.session && req.session.login){
+        res.render({sess: req.session.namelog});
+    }
+    return;
+})
+
+
